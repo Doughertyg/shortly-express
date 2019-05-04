02862f9d4857db2834ecf785bcabc1c3673b2f09
doughertyg <doughertyg37@gmail.com>
2019-05-04 12:30:31 -0700

    createSession in progress, some tests passing

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,9 +2,21 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-    if (!req.cookies.shortlyid) {
-        models.Sessions.create()
-            .then(next());
+    if (req.cookies !== undefined) {
+        if (req.cookies.shortlyid === undefined) {
+            models.Sessions.create()
+                .then((result) => {
+                    return models.Sessions.get({id: result.insertId})
+                })
+                .then(({ hash }) => {
+                    req.session = { "hash": hash };
+                    res.cookies = { "shortlyid": {value: hash } };
+                    next();
+                });
+        } else {
+            req.session = { hash: req.cookies.shortlyid };
+            next();
+        }
     }
     else next();
 };
